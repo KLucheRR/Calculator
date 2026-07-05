92ce5f6adab54e43e906b268df5b330d40b710cb
Kostya <Kostya@DESKTOP-NEU6I0E>
2026-07-05 23:36:36 +0300

    Реализованы базовые методы калькулятора

---
+++ Calculator/CalculatorCore/Calculator.cs
@@ -0,0 +1,46 @@
+﻿namespace CalculatorCore
+{
+
+    public static class Calculator
+    {
+        public static double Sum(params double[] args)
+        {
+            double result = 0;
+            foreach (var arg in args)
+            {
+                result += arg;
+            }
+            return result;
+        }
+
+        public static double Substract(params double[] args)
+        {
+            double result = args[0];
+            for (int i = 1; i < args.Length; i++)
+            {
+                result -= args[i];
+            }
+            return result;
+        }
+
+        public static double Multiply(params double[] args)
+        {
+            double result = 0;
+            foreach (var arg in args)
+            {
+                result *= arg;
+            }
+            return result;
+        }
+
+        public static double Divide(params double[] args)
+        {
+            double result = args[0];
+            for (int i = 1; i < args.Length; i++)
+            {
+                result /= args[i];
+            }
+            return result;
+        }
+    }
+}
